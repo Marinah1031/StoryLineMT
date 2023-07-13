834e1847c6424ce7f963665e93004a035cea3ed2
Marinah Tamura <marinahtam@gmail.com>
2023-07-12 22:08:09 -0700

    working on creating returns for diary entries

---
+++ helpers.js
@@ -0,0 +1,13 @@
+module.exports = {
+    datetime: (date) => {
+        const d = new Date(date).toISOString()
+        const array = d.split('-')
+        const year = array[0]
+        const month = array[1]
+        const dayAndTime = array[2]
+        const time = dayAndTime.split('T')[1]
+        const day = dayAndTime.split('T')[0]
+        return `${day}, ${time}
+        `
+    }
+}
