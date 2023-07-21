51135bae9b77633f801021bb167476eced62fd9e
kimberlyc <kimberlyc1904@yahoo.com>
2023-07-20 17:58:27 -0700

    removed Models folder

--- Models/index.js
+++
@@ -1,9 +0,0 @@
-// models/index.js
-const User = require('./User');
-const Diary = require('./Diary');
-
-// Define associations between models if needed
-User.hasMany(Diary, { foreignKey: 'user_id' });
-Diary.belongsTo(User, { foreignKey: 'user_id' });
-
-module.exports = { User, Diary };
